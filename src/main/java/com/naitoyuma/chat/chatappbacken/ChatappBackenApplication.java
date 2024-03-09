83e93bbdcfd04f1aefa52c27f82e3a086358947c
naitoyuma7110 <naitoyuma1118@gmail.com>
2024-03-10 02:51:15 +0900

    first commit

---
+++ src/main/java/com/naitoyuma/chat/chatappbacken/ChatappBackenApplication.java
@@ -0,0 +1,13 @@
+package com.naitoyuma.chat.chatappbacken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatappBackenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatappBackenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/naitoyuma/chat/chatappbacken/ChatappBackenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naitoyuma.chat.chatappbacken;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatappBackenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
