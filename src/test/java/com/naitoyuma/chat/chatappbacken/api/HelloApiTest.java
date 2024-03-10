406c35efa44fc01e263f79b920ed18c1cce7a311
naitoyuma7110 <naitoyuma1118@gmail.com>
2024-03-10 12:48:47 +0900

    controllerとDBに対するサンプルテスト

--- src/test/java/com/naitoyuma/chat/chatappbacken/api/HelloApiTest.java
+++ src/test/java/com/naitoyuma/chat/chatappbacken/api/HelloApiTest.java
@@ -9,17 +9,36 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.dbunit.Assertion;
+import org.dbunit.DataSourceDatabaseTester;
+import org.dbunit.IDatabaseTester;
+import org.dbunit.dataset.csv.CsvURLDataSet;
+import java.net.URL;
+
+// import jakarta.activation.DataSource;
+import javax.sql.DataSource;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 public class HelloApiTest {
-  // この2行でテスト用のコントローラーを含むMVC環境をMockMvcとしてインスタント化しテストで使用可能にする…
+  // 各種コントローラーを含むMVC環境をMockMvcとしてモック化しテスト用のメソッドで簡易的に使用可能にする
   @Autowired
   private MockMvc mockMvc;
 
-  // Controllerのモックを使用しリクエストとレスポンスの検証を行う(modelやserviceなど他レイヤーとは切り離されている)
+  // 各種DBテーブルを含むDBをDataSourceとしてモック化しテスト用のメソッドで簡易的に使用可能にする
+  @Autowired
+  private DataSource dataSource;
+
   @Test
   public void helloTest() throws Exception {
+
+    // Given、テスト実行に必要なレコードをDBのモックにセットする
+    IDatabaseTester databaseTester = new DataSourceDatabaseTester(dataSource);
+    URL givenUrl = this.getClass().getResource("/hello/hello/default/given/");
+    databaseTester.setDataSet(new CsvURLDataSet(givenUrl));
+    databaseTester.onSetup();
+
+    // Controllerのモックを使用しリクエストとレスポンスの検証を行う(modelやserviceなど他レイヤーとは切り離されている)
     mockMvc.perform(
         MockMvcRequestBuilders.get("/hello?name=naitoyuma")
             .accept(MediaType.APPLICATION_JSON))
@@ -31,6 +50,17 @@ public class HelloApiTest {
             """,
             result.getResponse().getContentAsString(),
             false));
+
+    // DBのモックを使用しDBデータの整合性の検証を行う(Controllerやserviceなど他レイヤーとは切り離されている)
+    var actualDataSet = databaseTester.getConnection().createDataSet();
+    // テスト実行後のDBレコード
+    var actualTestTable = actualDataSet.getTable("test");
+    var expectedUrl = this.getClass().getResource("/hello/hello/default/expected/");
+    var expectedDataSet = new CsvURLDataSet(expectedUrl);
+    // 期待するDBレコード
+    var expectedTestTable = expectedDataSet.getTable("test");
+    // 比較
+    Assertion.assertEquals(expectedTestTable, actualTestTable);
   }
 
 }
