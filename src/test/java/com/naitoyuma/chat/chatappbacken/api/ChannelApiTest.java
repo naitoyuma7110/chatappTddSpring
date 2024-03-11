c0005670cf094a119f41dfc4320e8a4888daaf9b
naitoyuma7110 <naitoyuma1118@gmail.com>
2024-03-11 18:01:43 +0900

    [TDD:Green]channelテスト修正、各種Postパラムをプロバイダーから受け取る

--- src/test/java/com/naitoyuma/chat/chatappbacken/api/ChannelApiTest.java
+++ src/test/java/com/naitoyuma/chat/chatappbacken/api/ChannelApiTest.java
@@ -1,6 +1,10 @@
 package com.naitoyuma.chat.chatappbacken.api;
 
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -28,37 +32,48 @@ public class ChannelApiTest {
                 """,
             result.getResponse().getContentAsString(),
             false));
-
   }
 
   @SuppressWarnings("null")
-  @Test
-  public void channelPostTest() throws Exception {
-
-    String jsonPayload = """
-        {
-            "id": 1,
-            "name": "テスト"
-        }
-        """;
+  @ParameterizedTest
+  @MethodSource("channelTestProvider")
+  public void channelPostTest(String queryString, String expectedBody) throws Exception {
 
     mockMvc.perform(
         MockMvcRequestBuilders.post("/channels")
-            .content(jsonPayload)
+            .content(
+                queryString)
             .contentType(
                 MediaType.APPLICATION_JSON)
             .accept(
                 MediaType.APPLICATION_JSON_UTF8))
         .andExpect(MockMvcResultMatchers.status().isOk())
         .andExpect((result) -> JSONAssert.assertEquals(
+            expectedBody,
+            result.getResponse().getContentAsString(),
+            false));
+  }
+
+  // Helloテストに各種引き数を渡すproviderメソッド
+  private static Stream<Arguments> channelTestProvider() {
+    return Stream.of(
+        // Stream型で各種パラメータを渡しテストを実行する
+        // テストはパラメータ毎に管理され選択して実行できる
+        Arguments.arguments(
             """
                 {
                     "id": 1,
                     "name": "テスト"
                 }
                 """,
-            result.getResponse().getContentAsString(),
-            false));
+            """
+                {
+                    "id": 1,
+                    "name": "テスト"
+                }
+                """)
+
+    );
 
   }
 
