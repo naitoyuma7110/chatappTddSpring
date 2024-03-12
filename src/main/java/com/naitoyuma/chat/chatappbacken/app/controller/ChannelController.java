42f382f878b8e8cc9f4fe0ec4907e80eedda40b8
naitoyuma7110 <naitoyuma1118@gmail.com>
2024-03-13 03:04:34 +0900

    RequiredArgsConstructorを使用したConstractorの省略、DIは自動で行われる

--- src/main/java/com/naitoyuma/chat/chatappbacken/app/controller/ChannelController.java
+++ src/main/java/com/naitoyuma/chat/chatappbacken/app/controller/ChannelController.java
@@ -1,9 +1,7 @@
 package com.naitoyuma.chat.chatappbacken.app.controller;
 
-import java.util.Collections;
 import java.util.List;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,16 +11,15 @@ import org.springframework.web.bind.annotation.RestController;
 import com.naitoyuma.chat.chatappbacken.app.service.ChannelService;
 import com.naitoyuma.chat.chatappbacken.domain.channels.model.Channel;
 
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/channels")
-@CrossOrigin
 public class ChannelController {
 
+  // コンストラクタが 1 つしかない場合、コンストラクタの@Autowiredを省略しても constructor injection が実行される
   private final ChannelService channelService;
-
-  public ChannelController(ChannelService channelService) {
-    this.channelService = channelService;
-  }
 
   @PostMapping()
   public Channel create(@RequestBody Channel channel) {
