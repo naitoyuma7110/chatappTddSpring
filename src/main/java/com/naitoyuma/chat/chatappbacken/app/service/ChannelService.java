51e1b6079df61e5972c48ac005ef2abbfa8b57e2
naitoyuma7110 <naitoyuma1118@gmail.com>
2024-03-13 03:03:41 +0900

    ServiceはChannel APIにおけるユースケース(ユーザーのアクション)に応じた操作を提供する

--- src/main/java/com/naitoyuma/chat/chatappbacken/app/service/ChannelService.java
+++ src/main/java/com/naitoyuma/chat/chatappbacken/app/service/ChannelService.java
@@ -1,23 +1,28 @@
 package com.naitoyuma.chat.chatappbacken.app.service;
 
-import java.util.Collections;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.naitoyuma.chat.chatappbacken.domain.channels.model.Channel;
+import com.naitoyuma.chat.chatappbacken.domain.service.ChannelDomainService;
 
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @Service
 @Transactional // 自動でメソッドにトランザクション、例外時のロールバックが実行される
 public class ChannelService {
 
+  private final ChannelDomainService channelDomainService;
+
   public Channel create(Channel channel) {
-    return channel;
+    return channelDomainService.create(channel);
   }
 
   public List<Channel> findAll() {
-    return Collections.emptyList();
+    return channelDomainService.findAll();
   }
 
 }
