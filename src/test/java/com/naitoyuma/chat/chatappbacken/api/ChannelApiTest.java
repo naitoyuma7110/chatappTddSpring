cd646499426954be66684950f7c642cf8c3707d4
naitoyuma7110 <naitoyuma1118@gmail.com>
2024-03-21 17:52:07 +0900

    [TDD:Red]Channel createに対するリクエストバリデーション、1～30文字、Notblank制約違反に対し400を返す想定でテスト作成

--- src/test/java/com/naitoyuma/chat/chatappbacken/api/ChannelApiTest.java
+++ src/test/java/com/naitoyuma/chat/chatappbacken/api/ChannelApiTest.java
@@ -76,19 +76,7 @@ public class ChannelApiTest {
                 "id": 3,
                 "name": "3回目のcreateテスト"
             }
-            """),
-        Arguments.arguments(
-            """
-                {
-                    "name": "errrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr"
-                }
-                """,
-            """
-                {
-                    "id": 4,
-                    "name": "errrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr"
-                }
-                """)
+            """)
 
     );
   }
@@ -96,9 +84,9 @@ public class ChannelApiTest {
   @SuppressWarnings("null")
   @ParameterizedTest
   @MethodSource("invalidChannelTestProvider")
-  public void invalidChannelPostTest(String queryString, String expectedErrorMessage)
-      throws Exception {
+  public void invalidChannelPostTest(String queryString) throws Exception {
 
+    //
     mockMvc
         .perform(MockMvcRequestBuilders.post("/channels").content(queryString)
             .contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON_UTF8))
@@ -107,6 +95,22 @@ public class ChannelApiTest {
     // MockMvcResultMatchers.content().string("{列 \"NAME CHARACTER VARYING(30)\" の値が長過ぎます"));
   }
 
+
+  private static Stream<Arguments> invalidChannelTestProvider() {
+    return Stream.of(
+
+        Arguments.arguments("""
+            {
+                "name": "30文字以上のメッセージですううううううううううううううううううううううううううううううううううううううううううううううううう"
+            }
+            """), Arguments.arguments("""
+            {
+                "name": ""
+            }
+            """)
+
+    );
+  }
 
 
 }
