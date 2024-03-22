97a42d255c64214f850f308b9fb925a9ef1f564f
naitoyuma7110 <naitoyuma1118@gmail.com>
2024-03-22 17:33:16 +0900

    [TDD:Green]Notnullバリデーション追加

--- src/main/java/com/naitoyuma/chat/chatappbacken/domain/channels/model/Channel.java
+++ src/main/java/com/naitoyuma/chat/chatappbacken/domain/channels/model/Channel.java
@@ -1,14 +1,15 @@
 package com.naitoyuma.chat.chatappbacken.domain.channels.model;
 
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class Channel {
+
   private int id;
 
-  @NotBlank
+  @NotNull(message = "Name is required")
   @Size(min = 1, max = 30, message = "Name must be between 1 and 30 characters")
   private String name;
 }
